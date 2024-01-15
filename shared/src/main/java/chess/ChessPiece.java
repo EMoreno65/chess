8e56eaaaa8713ee237946d0d6672ee7172b0778b
Ethan Moreno <ethanmm4@byu.edu>
2024-01-15 12:47:42 -0700

    First Commit - Made some global variables, pieceMoves and pass first test case is next

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -9,7 +10,6 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
-
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
     }
 
@@ -47,6 +47,6 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        return new ArrayList<>();
     }
 }
