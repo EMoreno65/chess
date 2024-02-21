6da20c780dbc05e034fb19fbfb6a5822021a54b4
Ethan Moreno <ethanmm4@byu.edu>
2024-02-21 16:12:49 -0700

    Created all the endpoints and created files for the handler and service classes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -8,9 +8,15 @@ public class Server {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
-        //Spark.post("/user", (req, res) -> (new RegisterHandler()).register(req, res));
+        Spark.post("/user", (req, res) -> (new RegisterHandler()).register(req, res));
+        Spark.delete("/db", (req, res) -> (new ClearHandler()).clear(req, res));
+        Spark.post("/session", (req, res) -> (new LoginHandler()).login(req, res));
+        Spark.delete("/session", (req, res) -> (new LogoutHandler()).logout(req, res));
+        Spark.get("/game", (req, res) -> (new ListHandler()).list(req, res));
+        Spark.post("/game", (req, res) -> (new CreateHandler()).create(req, res));
+        Spark.put("/game", (req, res) -> (new JoinHandler()).join(req, res));
 
-        // Register your endpoints and handle exceptions here.
+        //Register your endpoints and handle exceptions here.
 
         Spark.awaitInitialization();
         return Spark.port();
