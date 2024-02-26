11cfb040ee30a0f823164c7d15541063605a8379
Ethan Moreno <ethanmm4@byu.edu>
2024-02-26 13:45:36 -0700

    Chipping away at Login

--- server/src/main/java/server/LoginHandler.java
+++ server/src/main/java/server/LoginHandler.java
@@ -1,10 +1,20 @@
 package server;
 
+import RequestandResult.LoginRequest;
+import RequestandResult.LoginResult;
+import RequestandResult.RegisterRequest;
+import com.google.gson.Gson;
+import dataAccess.UserDAO;
+import service.LoginService;
 import spark.Request;
 import spark.Response;
 
 public class LoginHandler {
-  public Object login(Request req, Response res) {
-    return null;
+
+  LoginService myService = new LoginService();
+  public Object login(Request req, Response res, UserDAO userDAO) {
+    String myJSON =req.body();
+    LoginRequest givenRequest = new Gson().fromJson(myJSON, LoginRequest.class);
+    LoginResult myResult = myService.newResult()
   }
 }
--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,20 @@
-package server;
+package service;
+
+import RequestandResult.LoginRequest;
+import RequestandResult.LoginResult;
+import dataAccess.UserDAO;
 
 public class LoginService {
+  //  UserDAO userAccess = new UserDAO();
+//  AuthDAO tokenAccess = new AuthDAO();
+  public LoginResult newResult(LoginRequest givenRequest, UserDAO userDAO) {
+    UserData user = new UserData(givenRequest.getUsername(), givenRequest.getPassword(), givenRequest.getEmail());
+    try{
+      userDAO.createUser(user);
+      AuthData givenToken = authDAO.createAuth(user);
+      return new RegisterResult(givenToken.getAuthToken(), givenToken.getusername());
+    } catch(DataAccessException e) {
+      return new RegisterResult(e.getMessage());
+    }
+  }
 }
