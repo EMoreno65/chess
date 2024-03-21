a70a776c7e5d453fb2e1b446e98115d8177dd94c
Ethan Moreno <ethanmm4@byu.edu>
2024-03-21 12:33:46 -0600

    Wrote my serverFacade, I think it's right. Let's find out I guess

--- client/src/main/java/serverFacade/serverFacade.java
+++ client/src/main/java/serverFacade/serverFacade.java
@@ -4,10 +4,13 @@ package serverFacade;
 // Login, Register, Create, Join, etc
 // They're gonna contact my server over http
 
+import Request.*;
 import com.google.gson.Gson;
 import dataAccess.ResponseException;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -26,7 +29,51 @@ public class serverFacade {
 
   // User host and port to contact server
   // Move server request folders to shared  java
-  public void register(){
+  public RegisterRequest register(RegisterRequest request) throws ResponseException {
+    var path = "/user";
+    return this.makeRequest("POST", "/user", request, RegisterRequest.class);
+    // contact the server
+    // Access request and execute request
+    // Send request to server
+  }
+  public LoginRequest login(LoginRequest request) throws ResponseException {
+    var path = "/session";
+    return this.makeRequest("POST", "/session", request, LoginRequest.class);
+    // contact the server
+    // Access request and execute request
+    // Send request to server
+  }
+  public CreateRequest create(CreateRequest request) throws ResponseException {
+    var path = "/game";
+    return this.makeRequest("POST","/game", request, CreateRequest.class);
+    // contact the server
+    // Access request and execute request
+    // Send request to server
+  }
+  public JoinRequest join(JoinRequest request) throws ResponseException{
+    var path = "/game";
+    return this.makeRequest("PUT", "/game", request, JoinRequest.class);
+    // contact the server
+    // Access request and execute request
+    // Send request to server
+  }
+  public ListRequest list(ListRequest request) throws ResponseException{
+    var path = "/game";
+    return this.makeRequest("GET", "/game", request, ListRequest.class);
+    // contact the server
+    // Access request and execute request
+    // Send request to server
+  }
+  public ClearRequest clear(ClearRequest request) throws ResponseException{
+    var path = "/db";
+    return this.makeRequest("DELETE", "/db", request, ClearRequest.class);
+    // contact the server
+    // Access request and execute request
+    // Send request to server
+  }
+  public LogoutRequest logout(LogoutRequest request) throws ResponseException{
+    var path = "/session";
+    return this.makeRequest("DELETE", "/session", request, LogoutRequest.class);
     // contact the server
     // Access request and execute request
     // Send request to server
@@ -56,6 +103,29 @@ public class serverFacade {
         reqBody.write(reqData.getBytes());
       }
     }
+  }
+  private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+    T response = null;
+    if (http.getContentLength() < 0) {
+      try (InputStream respBody = http.getInputStream()) {
+        InputStreamReader reader = new InputStreamReader(respBody);
+        if (responseClass != null) {
+          response = new Gson().fromJson(reader, responseClass);
+        }
+      }
+    }
+    return response;
+  }
+
+  private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+    var status = http.getResponseCode();
+    if (!isSuccessful(status)) {
+      throw new ResponseException(status, "failure: " + status);
+    }
+  }
+
+  private boolean isSuccessful(int status) {
+    return status / 100 == 2;
   }
 
 }
--- client/src/main/java/ui/createMenu.java
+++ client/src/main/java/ui/createMenu.java
@@ -1,4 +1,11 @@
 package ui;
 
 public class createMenu {
+  // Print things to the terminal for the user to select
+
+  // some kinda switch statement
+  // login -> give username and password, put inside login request
+  // give login request to server facade call .login
+  // Print out results in terminal by calling methods and receiving objects
+
 }
